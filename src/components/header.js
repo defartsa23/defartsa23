66949837cf16a1ecb7f84efeef79047810974210
Deza Farras Tsany <deza.ftsany@gmail.com>
2022-06-25 00:11:04 +0700

    data diri

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,8 @@
 import * as React from "react"
 import PropTypes from "prop-types"
+import mail from "../images/email.png" 
+import linkedin from "../images/linkedin.png" 
+import github from "../images/github.png" 
 
 const Header = ({ greetings }) => {
   return (
@@ -9,7 +12,11 @@ const Header = ({ greetings }) => {
           <strong>Farras</strong> Deza Farras Tsany
         </h1>
         <span className="job-title">Software Engineer</span>
-        <span className="email">i@defartsa.dev</span>
+        <span className="email"> 
+          <a href="https://github.com/defartsa23" target={'_blank'}><img src={github} width={20}/></a>
+          <a href="https://www.linkedin.com/in/deza-farras-tsany/" target={'_blank'} style={{padding:10}}><img src={linkedin} width={20}/></a>
+          <a href="mailto:deza.ftsany@gmail.com" target={'_blank'}><img src={mail} width={20}/></a>
+        </span>
         <h2 className="lead">{greetings}</h2>
       </div>
     </div>
